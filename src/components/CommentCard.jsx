2e02dadcaf4545862f32ac73449fc035dc7b6d75
simongbarnes <simongbarnes@gmail.com>
2023-10-25 11:15:22 +0100

    5 replaced divs with semantic tags in CommentCard

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -5,30 +5,22 @@ export default function CommentCard(commentArg) {
 
   return (
     <>
-      <div className="comment-container">
-        <div className="comment-author">
-          <div>by {commentArg.comment.author}</div>
-        </div>
-        <div className="comment-date">
-          <div>{convertTimeStamp(commentArg.comment.created_at)}</div>
-        </div>
-        <article className="comment-body">
-          <div>{commentArg.comment.body}</div>
-        </article>
+      <section className="comment-container">
+          <p className="comment-author">by {commentArg.comment.author}</p>
+          <time className="comment-date">{convertTimeStamp(commentArg.comment.created_at)}</time>
+          <main className="comment-body">{commentArg.comment.body}</main>
         <div className="comment-pad-left"></div>
-        <div className="comment-vote-icon">
+        <figure className="comment-vote-icon">
           <img
             src={votesImg}
             alt="a thumbs up icon"
             width="20px"
             height="20px"
           ></img>
-        </div>
-        <div className="comment-vote-count">
-          <div>{commentArg.comment.votes}</div>
-        </div>
+        </figure>
+          <p className="comment-vote-count">{commentArg.comment.votes}</p>
         <div className="comment-pad-right"></div>
-      </div>
+      </section>
     </>
   );
 }
