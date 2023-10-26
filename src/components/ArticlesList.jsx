8f406729913b68658630d66640be6ae91d8db297
simongbarnes <simongbarnes@gmail.com>
2023-10-26 16:52:42 +0100

    10 sort articles

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -2,31 +2,38 @@ import { useEffect, useState } from "react";
 import ArticleCard from "./ArticleCard";
 import fetchArticles from "../utils/fetchArticles";
 import TopicSelector from "./TopicSelector";
+import SortSelector from "./SortSelector";
 
-export default function ArticlesList({topic}) {
+export default function ArticlesList({ topic }) {
   const [articles, setArticles] = useState([]);
   const [loading, setIsLoading] = useState(true);
   let queries = {};
 
   const [currentTopic, setCurrentTopic] = useState(topic);
+  const [currentSort, setCurrentSort] = useState('created_at');
+  const [currentOrder, setCurrentOrder] = useState('desc');
 
   queries.topic = currentTopic;
+  queries.sort = currentSort;
+  queries.order = currentOrder;
 
   useEffect(() => {
     fetchArticles(queries).then((response) => {
       setArticles(response.data.articles);
       setIsLoading(false);
-      return
-    })
-  }, [currentTopic]);
+      return;
+    });
+  }, [currentTopic, currentSort, currentOrder]);
 
   if (loading) return <p>Loading...</p>;
 
   return (
     <>
       <TopicSelector
-        currentTopic={currentTopic} setCurrentTopic={setCurrentTopic}
+        currentTopic={currentTopic}
+        setCurrentTopic={setCurrentTopic}
       />
+      <SortSelector currentSort={currentSort} setCurrentSort={setCurrentSort} currentOrder={currentOrder} setCurrentOrder={setCurrentOrder} />
       <ul>
         {articles.map((article, index) => {
           return (
---
+++ src/components/SortSelector.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+export default function SortSelector({ currentSort, setCurrentSort, currentOrder, setCurrentOrder }) {
+  const sortOptions = [
+    { column: "title", label: "title" },
+    { column: "author", label: "author" },
+    { column: "created_at", label: "date" },
+    { column: "votes", label: "votes" },
+    { column: "comment_count", label: "comments" },
+  ];
+
+  return (
+    <>
+      <form>
+        <label htmlFor="selected-sort">sort by: </label>
+        <select
+          name="sort"
+          id="sort"
+          // value={"created_at"}
+          onChange={(event) => {
+            setCurrentSort(event.target.value);
+          }}
+        >
+          {sortOptions.map((sortOption) => {
+            return (
+              <option key={sortOption.column} value={sortOption.column}>
+                {sortOption.label}
+              </option>
+            );
+          })}
+        </select>
+        <select
+          name="order"
+          id="order"
+          // value={"desc"}
+          onChange={(event) => {
+            setCurrentOrder(event.target.value);
+          }}
+        >
+          <option key={"asc"} value="asc">
+            ascending
+          </option>
+          <option key={"desc"} value="desc">
+            descending
+          </option>
+        </select>
+      </form>
+    </>
+  );
+}
